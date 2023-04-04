3d7c48143dd95c4abb04e3069d5ec7824c2bb1e8
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2023-04-04 22:31:25 +0300

    добавление модулей

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/App.jsx';
 
 
-const hi = <h2>Hello</h2>; // jsx / если многострочный элемент, оборачиваем его в скобки
-// всегда один родитель
-console.log(hi);
+// const hi = <h2>Hello</h2>; // jsx / если многострочный элемент, оборачиваем его в скобки
+// // всегда один родитель
+// console.log(hi);
 
 // const elem = React.createElement('h3', { className: 'greeting' }, 'hello people'); // тег, класс, содержимое
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-
+        <App/>
     </React.StrictMode>,
 );
