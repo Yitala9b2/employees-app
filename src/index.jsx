a8e8be42f9c5ea1acd6046d20915250e90eb460f
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2023-04-05 18:49:22 +0300

    удалил коммент

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/App.jsx';
 
 
-// const hi = <h2>Hello</h2>; // jsx / если многострочный элемент, оборачиваем его в скобки
-// // всегда один родитель
-// console.log(hi);
-
-// const elem = React.createElement('h3', { className: 'greeting' }, 'hello people'); // тег, класс, содержимое
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
